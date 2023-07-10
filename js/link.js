67a7ab41d6582e75e1a0449c202de603032f71b0
Vladimir Tinica <vltinica@gmail.com>
2023-07-10 09:50:03 -0400

    push

---
+++ js/link.js
@@ -0,0 +1,35 @@
+// Sushi link
+document.getElementById("btn_1").addEventListener("click", f1);
+function f1() {
+  return (window.location.href = "../portfolio-websites/sushi/index.html");
+}
+
+// Hospital link
+document.getElementById("btn_2").addEventListener("click", f2);
+function f2() {
+  return (window.location.href = "../portfolio-websites/hospital/index.html");
+}
+
+// Shopping link
+document.getElementById("btn_3").addEventListener("click", f3);
+function f3() {
+  return (window.location.href = "../portfolio-websites/clothes/index.html");
+}
+
+// Translation link
+document.getElementById("btn_4").addEventListener("click", f4);
+function f4() {
+  return (window.location.href = "../portfolio-websites/coffee/index.html");
+}
+
+//Online link
+document.getElementById("btn_5").addEventListener("click", f5);
+function f5() {
+  return (window.location.href = "../portfolio-websites/headphones/index.html");
+}
+
+// Delivery link
+document.getElementById("btn_6").addEventListener("click", f6);
+function f6() {
+  return (window.location.href = "../portfolio-websites/Delivery/index.html");
+}
---
+++ portfolio-websites/coffee/assets/js/main.js
@@ -0,0 +1,60 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  this.scrollY >= 50
+    ? header.classList.add("scroll-header")
+    : header.classList.remove("scroll-header");
+};
+window.addEventListener("scroll", scrollHeader);
+
+/*=============== SHOW SCROLL UP ===============*/
+
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 200) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  // reset:true //Animations repeat
+});
+
+sr.reveal(`.home__data, .products__data, .steps__content, .footer__container`);
+sr.reveal(`.home__img`, { origin: "bottom" });
+sr.reveal(`.products__card`, { interval: 100 });
+sr.reveal(`.about__img, .testimonial__img`, { origin: "right" });
+sr.reveal(`.about__data, .testimonial__data`, { origin: "left" });
